5f220810bc4a90ad3b6910439cc5c3f696dde01d
orxfun <orx.ugur.arikan@gmail.com>
2024-08-13 16:52:17 +0200

    clippy fix in impl from default macro

--- src/implementations/imp_from_default.rs
+++ src/implementations/imp_from_default.rs
@@ -2,14 +2,14 @@
 #[macro_export]
 macro_rules! impl_from_default {
     ($t:ty) => {
-        impl crate::PseudoDefault for $t {
+        impl $crate::PseudoDefault for $t {
             fn pseudo_default() -> Self {
                 Default::default()
             }
         }
     };
     ($a:ident, $t:ty) => {
-        impl<$a> crate::PseudoDefault for $t {
+        impl<$a> $crate::PseudoDefault for $t {
             fn pseudo_default() -> Self {
                 Default::default()
             }
